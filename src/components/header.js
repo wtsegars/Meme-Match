140668736410ffac4125df6a2e8e0aa050448f97
Darkangel <wtsegars@mac.com>
2019-06-21 00:07:00 -0400

    Added first react component

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div className="jumbotron">
+            <h1 class="display-4">Meme Match Game</h1>
+            <p class="lead">A Game of Matching Memes to Memes.</p>
+            <button class="btn btn-primary btn-lg"></button>
+        </div>
+    );
+}
+
+export default Header;
