920dfc276d362cdcb35203f90efa026da7fd68c7
Darkangel <wtsegars@mac.com>
2019-06-22 09:58:13 -0400

    Edited the Grid component of the React app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import Header from './header';
+import React, { Component } from 'react';
+import Header from 'header';
+import Grid from 'grid';
 
-function App() {
-  return (
-    <Header />
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Grid />
+      </div>
+    );
+  }
+  
 }
 
 export default App;
--- src/components/grid.js
+++ src/components/grid.js
@@ -1,7 +1,130 @@
 import React from 'react';
 
-function grid() {
+function Grid() {
     return(
-        
+        <div className="container">
+            <div className="row">
+                <div className="col-2">
+                    1
+                </div>
+                <div className="col-2">
+                    2
+                </div>
+                <div className="col-2">
+                    3
+                </div>
+                <div className="col-2">
+                    4
+                </div>
+                <div className="col-2">
+                    5
+                </div>
+                <div className="col-2">
+                    6
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-2">
+                    7
+                </div>
+                <div className="col-2">
+                    8
+                </div>
+                <div className="col-2">
+                    9
+                </div>
+                <div className="col-2">
+                    10
+                </div>
+                <div className="col-2">
+                    11
+                </div>
+                <div className="col-2">
+                    12
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-2">
+
+                </div>
+                <div className="col-2">
+
+                </div>
+                <div className="col-2">
+
+                </div>
+                <div className="col-2">
+
+                </div>
+                <div className="col-2">
+
+                </div>
+                <div className="col-2">
+
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-2">
+
+                </div>
+                <div className="col-2">
+
+                </div>
+                <div className="col-2">
+
+                </div>
+                <div className="col-2">
+
+                </div>
+                <div className="col-2">
+
+                </div>
+                <div className="col-2">
+
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-2">
+
+                </div>
+                <div className="col-2">
+
+                </div>
+                <div className="col-2">
+
+                </div>
+                <div className="col-2">
+
+                </div>
+                <div className="col-2">
+
+                </div>
+                <div className="col-2">
+
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-2">
+
+                </div>
+                <div className="col-2">
+
+                </div>
+                <div className="col-2">
+
+                </div>
+                <div className="col-2">
+
+                </div>
+                <div className="col-2">
+
+                </div>
+                <div className="col-2">
+
+                </div>
+            </div>
+        </div>
     );
 }
+
+export default Grid;
