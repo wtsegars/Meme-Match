9f9e260fa0dadb5a21b1d876d5c9027d11816d84
Darkangel <wtsegars@mac.com>
2019-06-22 11:18:40 -0400

    Created the scripts component

--- src/components/grid.js
+++ src/components/grid.js
@@ -4,123 +4,123 @@ function Grid() {
     return(
         <div className="container">
             <div className="row">
-                <div className="col-2">
+                <div className="col-2" id="1">
                     1
                 </div>
-                <div className="col-2">
+                <div className="col-2" id="2">
                     2
                 </div>
-                <div className="col-2">
+                <div className="col-2" id="3">
                     3
                 </div>
-                <div className="col-2">
+                <div className="col-2" id="4">
                     4
                 </div>
-                <div className="col-2">
+                <div className="col-2" id="5">
                     5
                 </div>
-                <div className="col-2">
+                <div className="col-2" id="6">
                     6
                 </div>
             </div>
             <div className="row">
-                <div className="col-2">
+                <div className="col-2" id="7">
                     7
                 </div>
-                <div className="col-2">
+                <div className="col-2" id="8">
                     8
                 </div>
-                <div className="col-2">
+                <div className="col-2" id="9">
                     9
                 </div>
-                <div className="col-2">
+                <div className="col-2" id="10">
                     10
                 </div>
-                <div className="col-2">
+                <div className="col-2" id="11">
                     11
                 </div>
-                <div className="col-2">
+                <div className="col-2" id="12">
                     12
                 </div>
             </div>
             <div className="row">
-                <div className="col-2">
+                <div className="col-2" id="13">
                     13
                 </div>
-                <div className="col-2">
+                <div className="col-2" id="14">
                     14
                 </div>
-                <div className="col-2">
+                <div className="col-2" id="15">
                     15
                 </div>
-                <div className="col-2">
+                <div className="col-2" id="16">
                     16
                 </div>
-                <div className="col-2">
+                <div className="col-2" id="17">
                     17
                 </div>
-                <div className="col-2">
+                <div className="col-2" id="18">
                     18
                 </div>
             </div>
             <div className="row">
-                <div className="col-2">
-
+                <div className="col-2" id="19">
+                    19
                 </div>
-                <div className="col-2">
-
+                <div className="col-2" id="20">
+                    20
                 </div>
-                <div className="col-2">
-
+                <div className="col-2" id="21">
+                    21
                 </div>
-                <div className="col-2">
-
+                <div className="col-2" id="22">
+                    22
                 </div>
-                <div className="col-2">
-
+                <div className="col-2" id="23">
+                    23
                 </div>
-                <div className="col-2">
-
-                </div>
-            </div>
-            <div className="row">
-                <div className="col-2">
-
-                </div>
-                <div className="col-2">
-
-                </div>
-                <div className="col-2">
-
-                </div>
-                <div className="col-2">
-
-                </div>
-                <div className="col-2">
-
-                </div>
-                <div className="col-2">
-
+                <div className="col-2" id="24">
+                    24
                 </div>
             </div>
             <div className="row">
-                <div className="col-2">
-
+                <div className="col-2" id="25">
+                    25
                 </div>
-                <div className="col-2">
-
+                <div className="col-2" id="26">
+                    26
                 </div>
-                <div className="col-2">
-
+                <div className="col-2" id="27">
+                    27
                 </div>
-                <div className="col-2">
-
+                <div className="col-2" id="28">
+                    28
                 </div>
-                <div className="col-2">
-
+                <div className="col-2" id="29">
+                    29
                 </div>
-                <div className="col-2">
-
+                <div className="col-2" id="30">
+                    30
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-2" id="31">
+                    31
+                </div>
+                <div className="col-2" id="32">
+                    32
+                </div>
+                <div className="col-2" id="33">
+                    33
+                </div>
+                <div className="col-2" id="34">
+                    34
+                </div>
+                <div className="col-2" id="35">
+                    35
+                </div>
+                <div className="col-2" id="36">
+                    36
                 </div>
             </div>
         </div>
---
+++ src/components/scripts.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Scripts() {
+    return(
+        <script src="js/click.js" type="text/javascript"></script>
+    );
+}
+
+export default Scripts;
