e530ac9ccb300ec8edc3c20ddb9c8c8f7f1ceb81
Darkangel <wtsegars@mac.com>
2019-06-23 21:37:06 -0400

    Edited front end js

--- public/js/click.js
+++ public/js/click.js
@@ -12,13 +12,23 @@ $(document).ready(function() {
     const scumbagSteve = "images/scumbagsteve.jpg";
     const triggeredFem = "images/triggeredfem.jpg";
     const trollFace = "images/TrollFace.jpg";
+    const bbqBecky = "images/bbqbecky.jpg";
+    const benDestroysLib = "images/bendestroyeslib.png";
+    const distractedBoyfriend = "images/distractedboyfriend.jpg";
+    const earthChan = "images/earthchan.jpeg";
+    const iDontFeelSoGood = "images/idontfeelsogood.jpg";
+    const inhaleSeagull = "images/inhaleseagull.jpg";
+    const mockingSpongebob = "images/mockingspongebob.jpg";
+    const savagePatrick = "images/savagepatrick.png";
+    const trumpetBoy = "images/trumpetboy.jpg";
+    const ugandanKnuckles = "images/ugandanknuckles.jpg";
+    const wallmartBoy = "images/wallmartboy.png";
 
-    let counter = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-    for(let i = 1; i < 36; i++){
-        $('#' + [i] +'')
+    const memeArray = [almostPcRedneck, bigChungus, bolbi, changeMyMind, doggo, foreverAlone, meNTheBoys, notSureIfFry, pepe, sarcasticWonka, scumbagSteve, triggeredFem, trollFace, bbqBecky, benDestroysLib, distractedBoyfriend, earthChan, iDontFeelSoGood, inhaleSeagull, mockingSpongebob, savagePatrick, trumpetBoy, ugandanKnuckles, wallmartBoy];
+
+    for (let i = 0; i < memeArray.length; i++){
+        for (let j = 0; j < 36; j++){
+            
+        }
     }
-
-    $(document).onClick('.clickBox', function() {
-        
-    })
 })
--- src/components/grid.js
+++ src/components/grid.js
@@ -5,122 +5,82 @@ function Grid() {
         <div className="container">
             <div className="row">
                 <div className="col-2 clickBox" id="1">
-                    1
+                    
                 </div>
                 <div className="col-2 clickBox" id="2">
-                    2
+                    
                 </div>
                 <div className="col-2 clickBox" id="3">
-                    3
+                    
                 </div>
                 <div className="col-2 clickBox" id="4">
-                    4
+                    
                 </div>
                 <div className="col-2 clickBox" id="5">
-                    5
+                    
                 </div>
                 <div className="col-2 clickBox" id="6">
-                    6
+                    
                 </div>
             </div>
             <div className="row">
                 <div className="col-2 clickBox" id="7">
-                    7
+                    
                 </div>
                 <div className="col-2 clickBox" id="8">
-                    8
+                    
                 </div>
                 <div className="col-2 clickBox" id="9">
-                    9
+                    
                 </div>
                 <div className="col-2 clickBox" id="10">
-                    10
+                    
                 </div>
                 <div className="col-2 clickBox" id="11">
-                    11
+                    
                 </div>
                 <div className="col-2 clickBox" id="12">
-                    12
+                    
                 </div>
             </div>
             <div className="row">
                 <div className="col-2 clickBox" id="13">
-                    13
+                    
                 </div>
                 <div className="col-2 clickBox" id="14">
-                    14
+                    
                 </div>
                 <div className="col-2 clickBox" id="15">
-                    15
+                    
                 </div>
                 <div className="col-2 clickBox" id="16">
-                    16
+                    
                 </div>
                 <div className="col-2 clickBox" id="17">
-                    17
+                    
                 </div>
                 <div className="col-2 clickBox" id="18">
-                    18
+                    
                 </div>
             </div>
             <div className="row">
                 <div className="col-2 clickBox" id="19">
-                    19
+                    
                 </div>
                 <div className="col-2 clickBox" id="20">
-                    20
+                    
                 </div>
                 <div className="col-2 clickBox" id="21">
-                    21
+                    
                 </div>
                 <div className="col-2 clickBox" id="22">
-                    22
+                    
                 </div>
                 <div className="col-2 clickBox" id="23">
-                    23
+                    
                 </div>
                 <div className="col-2 clickBox" id="24">
-                    24
-                </div>
-            </div>
-            <div className="row">
-                <div className="col-2 clickBox" id="25">
-                    25
-                </div>
-                <div className="col-2 clickBox" id="26">
-                    26
-                </div>
-                <div className="col-2 clickBox" id="27">
-                    27
-                </div>
-                <div className="col-2 clickBox" id="28">
-                    28
-                </div>
-                <div className="col-2 clickBox" id="29">
-                    29
-                </div>
-                <div className="col-2 clickBox" id="30">
-                    30
-                </div>
-            </div>
-            <div className="row">
-                <div className="col-2 clickBox" id="31">
-                    31
-                </div>
-                <div className="col-2 clickBox" id="32">
-                    32
-                </div>
-                <div className="col-2 clickBox" id="33">
-                    33
-                </div>
-                <div className="col-2 clickBox" id="34">
-                    34
-                </div>
-                <div className="col-2 clickBox" id="35">
-                    35
-                </div>
-                <div className="col-2 clickBox" id="36">
-                    36
+                    
                 </div>
             </div>
         </div>
